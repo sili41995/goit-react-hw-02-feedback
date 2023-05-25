c260eba5ff3b75f381c4b30ac14ac6a16476313e
Silich Alexander <sili41995@gmail.com>
2023-05-25 12:44:20 +0300

    new

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import FeedbackForm from 'components/FeedbackForm/FeedbackForm';
+
+const App = () => {
+  return (
+    <div>
+      <FeedbackForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -0,0 +1,49 @@
+import { Component } from 'react';
+
+class FeedbackForm extends Component {
+  addFeedback() {
+    console.log(1);
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <p>Please leave feedback</p>
+          <ul>
+            <li>
+              <button type='button' onClick={this.addFeedback}>
+                Goog
+              </button>
+            </li>
+            <li>
+              <button type='button' onClick={this.addFeedback}>
+                Neutral
+              </button>
+            </li>
+            <li>
+              <button type='button' onClick={this.addFeedback}>
+                Bad
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p>Statistics</p>
+          <ul>
+            <li>
+              Good: <span>0</span>
+            </li>
+            <li>
+              Neutral: <span>0</span>
+            </li>
+            <li>
+              Bad: <span>0</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FeedbackForm;
